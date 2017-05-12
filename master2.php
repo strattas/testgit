0050c1e0486b6c9f46a2bb171f091d1822a315ed
Jason Ratzlaff <jasonratz@gmail.com>
2017-05-11 21:18:36 -0700

    Add master 2 file to master branch

---
+++ master2.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @author     Guidance Magento Team <magento@guidance.com>
+ * @category   testgit
+ * @package    testgit_${MODULENAME}
+ * @copyright  Copyright (c) 2017 Guidance Solutions (http://www.guidance.com)
+ */
