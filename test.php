c8c9194deedf685bc69109f62bf7ed54917a4721
Jason Ratzlaff <jasonratz@gmail.com>
2017-05-11 20:15:52 -0700

    Added Test File

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author     Guidance Magento Team <magento@guidance.com>
+ * @category   testgit
+ * @package    testgit_${MODULENAME}
+ * @copyright  Copyright (c) 2017 Guidance Solutions (http://www.guidance.com)
+ */
+
+echo "HERE I The Test File";
